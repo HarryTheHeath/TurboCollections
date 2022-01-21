9860f8e4626458da6038764b139c7f646ae37163
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-21 22:52:25 +0100

    Added: Test: Stack class

--- TurboCollections.Test/TurboStackTests.cs
+++ TurboCollections.Test/TurboStackTests.cs
@@ -5,6 +5,28 @@ namespace TurboCollections.Test
     [TestFixture]
     public class TurboStackTests
     {
-        
+        [Test]
+        public void HasEmptyConstructor()
+        {
+            new TurboStack<int>();
+            Assert.Pass();
+        }
+
+        public class ANewStack
+        {
+            private TurboStack<int> stack;
+            
+            [SetUp]
+            public void SetUp()
+            {
+                this.stack = new TurboStack<int>();
+            }
+            
+            [Test]
+            public void HasACountOfZero()
+            {
+                Assert.Zero(stack.Count);
+            }
+        }
     }
 }
